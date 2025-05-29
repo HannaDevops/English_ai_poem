0873d18ed4b22571606d07ecac5d22ee1b895df8
HannaDevops <115883036+HannaDevops@users.noreply.github.com>
2025-05-29 20:29:32 +0100

    add api
    add axios

--- scr/script.js
+++ scr/script.js
@@ -1,11 +1,25 @@
+function showPoem(response){
+    new Typewriter('#poem', {
+        strings: response.data.answer,
+        autoStart: true,
+});
+}
+
 
 function createPoem(event){
     event.preventDefault();
-   
-    new Typewriter('#poem', {
-        strings: "space for poem",
-        autoStart: true,
-});
+    
+    let userPrompt =document.querySelector("#user-prompt");
+
+    let keyApi = "35af20a51228d76bt18bb4ac458c490o";   
+    let promptApi = `Create 4 line english poem about ${userPrompt.value}`;
+    let contextApi = "Make sure to provide a clear and precise answer  separate each line with a <br />";  
+    let urlApi = `https://api.shecodes.io/ai/v1/generate?prompt=${promptApi}&context=${contextApi}&key=${keyApi}`;
+    
+    
+    axios.get(urlApi).then(showPoem);
+
+    
 }
 
 
